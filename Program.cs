bb87a9a624f896bf39dfd000ce16d597efd64d48
connor <vbrookesy@hotmail.co.uk>
2020-05-12 15:39:15 +0100

    Started off with adding the 2 numbers together as the final writeline, users choice

--- Program.cs
+++ Program.cs
@@ -20,7 +20,9 @@ namespace ConsoleDotNetApp
             int num1 = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Enter another number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());                    
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
 
 
             
