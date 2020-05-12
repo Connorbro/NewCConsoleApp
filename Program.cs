b4b312182a220eecb7e41cff3146905c3e379d59
connor <vbrookesy@hotmail.co.uk>
2020-05-12 10:50:13 +0100

    Storing my favourite programming language

--- Program.cs
+++ Program.cs
@@ -8,11 +8,12 @@ namespace ConsoleDotNetApp
         {
             var name = "Connor";
             var age = 21;
+            string CodingLanguage = "C#";
 
 
             Console.WriteLine($"My name is {name}");
             Console.WriteLine($"I am {age} years old");
-            Console.WriteLine("I love C#");
+            Console.WriteLine($"I love {CodingLanguage}");
 
             Console.ReadLine();
         }
